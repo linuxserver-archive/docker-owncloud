88b16c7ef7372244b3645a6df8946608882c9211
sparklyballs <sparklyballs@gmail.com>
2016-02-08 12:23:53 +0000

    revert back to the original keygen

--- init/40_gen_ssl_keys.sh
+++ init/40_gen_ssl_keys.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
-[[ -f /config/keys/cert.key &&  -f /config/keys/cert.crt ]] && (echo "using keys found in /config/keys" && exit 0)
-
 SUBJECT="//C=US/ST=CA/L=Carlsbad/O=Linuxserver.io/OU=LSIO Server/CN=*"
+if [[ -f /config/keys/cert.key && -f /config/keys/cert.crt ]]; then
+echo "using keys found in /config/keys"
+else
 echo "generating self-signed keys in /config/keys, you can replace these with your own keys if required"
 openssl req -new -x509 -days 3650 -nodes -out /config/keys/cert.crt -keyout /config/keys/cert.key -subj "$SUBJECT"
+fi
 
-chown abc:abc -R /config/keys
 
 
