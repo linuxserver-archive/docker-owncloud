c5f2f2541e1169571c3f52f7a686790bbe0b8bda
sparklyballs <sparklyballs@gmail.com>
2016-02-09 19:35:35 +0000

    last play with the key init

--- init/40_gen_ssl_keys.sh
+++ init/40_gen_ssl_keys.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-set -e
-[[ -f /config/keys/cert.key && -f /config/keys/cert.crt ]] && (echo "using keys found in /config/keys" && chown abc:abc -R /config/keys && exit 0)
 
+if [[ -f /config/keys/cert.key && -f /config/keys/cert.crt ]]; then
+echo "using existing keys in \"/config/keys\""
+else
 echo "generating self-signed keys in /config/keys, you can replace these with your own keys if required"
 openssl req -new -x509 -days 3650 -nodes -out /config/keys/cert.crt -keyout /config/keys/cert.key -subj "//C=US/ST=CA/L=Carlsbad/O=Linuxserver.io/OU=LSIO Server/CN=*"
-chown abc:abc -R /config/keys
+fi
 
+chown abc:abc -R /config/keys
 
 
