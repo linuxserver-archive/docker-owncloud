76c1e3a7127899d18e08028a841ad592a80ec06d
sparklyballs <sparklyballs@gmail.com>
2016-02-14 15:27:42 +0000

    placeholder echo

--- init/70_memcache_setup.sh
+++ init/70_memcache_setup.sh
@@ -2,7 +2,8 @@
 
 [[ ! -f /config/www/owncloud/config/config.php ]] && exit 0
 
-# if [ "$(grep -c "memcache.local" "/config/www/owncloud/config/config.php")" == "0" ]; then
+ if [ "$(grep -c "memcache.local" "/config/www/owncloud/config/config.php")" == "0" ]; then
+echo "blah"
 # sed -i -e "'memcache.local' => '\OC\Memcache\APCu'," /config/www/owncloud/config/config.php
-# fi
+ fi
 
