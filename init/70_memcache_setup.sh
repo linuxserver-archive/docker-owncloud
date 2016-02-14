8b8fbe93626d551f1210ae2bfe52924782a3d332
sparklyballs <sparklyballs@gmail.com>
2016-02-14 15:21:29 +0000

    comment out memcache settings till get sed right

--- init/70_memcache_setup.sh
+++ init/70_memcache_setup.sh
@@ -2,7 +2,7 @@
 
 [[ ! -f /config/www/owncloud/config/config.php ]] && exit 0
 
-if [ "$(grep -c "memcache.local" "/config/www/owncloud/config/config.php")" == "0" ]; then
-sed -i -e "'memcache.local' => '\OC\Memcache\APCu'," /config/www/owncloud/config/config.php
-fi
+# if [ "$(grep -c "memcache.local" "/config/www/owncloud/config/config.php")" == "0" ]; then
+# sed -i -e "'memcache.local' => '\OC\Memcache\APCu'," /config/www/owncloud/config/config.php
+# fi
 
